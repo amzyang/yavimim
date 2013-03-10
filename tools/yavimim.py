e3c626e55de5a21669edb0ab053fa2634c8216af
kikyo <frederick.zou@gmail.com>
2013-03-10 13:11:15 +0800

    show input method when activated only

--- tools/yavimim.py
+++ tools/yavimim.py
@@ -5,12 +5,13 @@ import vim
 def yavimim_status():
     buffer_iminsert = int(vim.eval("&l:iminsert"))
     if buffer_iminsert != 1:
-        contents = "EN"
+        return None
     else:
         traditional = int(vim.eval("g:yavimim_traditional"))
-        sim_cht = '繁' if traditional else '简'
+        sim_cht = u'繁' if traditional else u'简'
         im = vim.eval('yavimim#backend#getim()')
         name = 'name_cht' if traditional else 'name'
-        contents = im[name] + "." + sim_cht
-    return [{'contents': contents, "highlight_group": ['yavimim_status']}]
+        contents = im[name].decode('utf8') + "." + sim_cht
+        contents = contents.encode('utf8')
+    return [{'contents': str(contents), "highlight_group": ['yavimim_status']}]
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 textwidth=79
